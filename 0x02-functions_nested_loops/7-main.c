bd98157bf4db85a2d0085cce6366aa7e9cb58ff7
Joyce Thuo <sherowsorganics@gmail.com>
2023-07-18 07:50:07 -0700

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/7-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	print_last_digit(98);
+	print_last_digit(0);
+	print_last_digit(-1024);
+	_putchar('\n');
+
+	return (0);
+}
+
